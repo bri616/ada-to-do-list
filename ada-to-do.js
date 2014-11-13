6de2d79dde59f9b64536de1529b6036b43eaf57c
Bri Dotson <bridotson@gmail.com>
2014-11-12 16:21:06 -0800

    make fade-in happen. make styling nicer

--- ada-to-do.js
+++ ada-to-do.js
@@ -3,12 +3,15 @@ $(function() {
 
   $("#remTask").click(remButtonClick);
 
+  $("#list").sortable();
+
+
 });
 
 function addButtonClick(event) {
   event.preventDefault();
   // append the input task as a list item to the list
-  $("#list").append("<li>"+ $("#task").val() +"</li>");
+  $("<li>"+ $("#task").val() +"</li>").appendTo("#list").hide().fadeIn("slow");
   // get bgcolor based on whether the element is even or odd
   var bgcolor = ($("li").length % 2 === 0 ? "grey" : "lightblue");
   // apply bgcolor name
